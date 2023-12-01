90a1ba59b2909c3f40c1b8c9aa40d9cb93479038
Bravo-7-9 <131817044+Bravo-7-9@users.noreply.github.com>
2023-11-30 20:31:11 -0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $message = $_POST["alert"];
+  $message = wordwrap($message,70);
+  mail("bravo-7-9@outlook.com","MCSCADA Alert", $message);
+}
+?>
